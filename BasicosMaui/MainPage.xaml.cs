585257e4beded3941a5aaf423dec36c139e479ff
jmot4 <jmota@gtech-services.com>
2026-05-08 15:54:30 -0600

    Se agregan cambios al 8 de mayo

--- BasicosMaui/MainPage.xaml.cs
+++ BasicosMaui/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace BasicosMaui
+﻿
+
+namespace BasicosMaui
 {
     public partial class MainPage : ContentPage
     {
@@ -9,6 +11,22 @@
             InitializeComponent();
         }
 
-       
+        private void CambioNombre(object sender,TextChangedEventArgs e) 
+        {
+            Console.WriteLine($"Nombre actual:{e.NewTextValue}");
+        }
+
+        private void OnSwicthToggled(object sender,ToggledEventArgs e) 
+        {
+            string estado = e.Value ? "Activadas" : "Desactivadas";
+            DisplayAlert("Switch", $"Notificaciones:{estado}", "Ok");
+        }
+
+        private void OnSliderChanged(object sender,ValueChangedEventArgs e)        
+        {
+            lblExperiencia.Text = $"Nivel: {(int)e.NewValue}";
+        }
+
+
     }
 }
