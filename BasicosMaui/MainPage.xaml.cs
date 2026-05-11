053b6b00a0aec97b430224a372385291e09a2982
jmot4 <jmota@gtech-services.com>
2026-05-11 15:38:19 -0600

    Basicos de Maui terminado.

--- BasicosMaui/MainPage.xaml.cs
+++ BasicosMaui/MainPage.xaml.cs
@@ -27,6 +27,23 @@ namespace BasicosMaui
             lblExperiencia.Text = $"Nivel: {(int)e.NewValue}";
         }
 
+        private async void OnGuardarClick(object sender, EventArgs e)
+        {
+            string nombre = entradaNombre.Text ?? "Sin Nombre";
+            string edad = entradaEdad.Text ?? "Sin Edad";
+            bool notif = switchNotificaciones.IsToggled;
+            int nivel = (int)slideExperiencia.Value;
+
+            lblResultado.Text =
+                $"Nombre:{nombre}\n" +
+                $"Edad: {edad}\n" +
+                $"Notificaciones: {(notif ? "Si" : "No")}\n" +
+                $"Nivel de Experiencia: {nivel}/10";
+
+            await DisplayAlert("¡Guardado!", "Datos registrados correctamente","Ok");
+
+        }
+
 
     }
 }
